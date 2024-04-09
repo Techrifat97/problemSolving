ccf79dfce5c441fdbcf5bad5b38bb3c1534c4cad
rifat ali shaon <rifatshaon.tech@gmail.com>
2024-04-09 23:52:40 +0200

    Updated-cocis1.py

--- coci17/coci_s1.py
+++ coci17/coci_s1.py
@@ -1,15 +1,17 @@
 wind_initial_obey = input()
 number_of_duels = int(input())
 duels = list()
-
+new_obey = wind_initial_obey
 
 for i in range(number_of_duels):
     duels.append(input().split())
+count = 1
+for item in duels:
+    if item[1] == new_obey:
+        new_obey = item[0]
+        if new_obey != wind_initial_obey:
+            count += 1
     
-
-for item in duels: 
-    if item[1] == wind_initial_obey:
-        currently_obeying = item[0]
-    
-        
-print(currently_obeying)
+print(new_obey)
+print(count)
+            
